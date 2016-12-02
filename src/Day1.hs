ef7ec6c76123ae86dd1b0e1447185a5bc634e1e1
Reid Wilbur <reid.wilbur@gmail.com>
2016-12-02 18:19:08 -0500

    Day1 part1 sol

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,4 +1,38 @@
-module Day1 (t1) where
+module Day1 (getHqDist, parseInput, parseStep, PathStep(R,L)) where
 
-t1 :: Integer -> Integer
-t1 i = i
+import Data.List.Split as Split
+import Data.List as List
+
+data PathStep = R Int
+              | L Int
+              deriving (Eq, Ord, Show)
+
+data Direction = N | S | E | W deriving (Eq, Ord, Show)
+
+type Position = (Direction, (Int, Int))
+
+parseStep :: String -> PathStep
+parseStep s = case s of
+                'L':d -> L (read d :: Int)
+                'R':d -> R (read d :: Int)
+                _ -> error ("could not parse step " ++ s)
+
+parseInput :: String -> [PathStep]
+parseInput "" = []
+parseInput ss = List.map parseStep $ Split.splitOn ", " ss
+
+getNextPos :: Position -> PathStep -> Position
+getNextPos (N, (x, y)) (R i) = (E, (x + i, y))
+getNextPos (N, (x, y)) (L i) = (W, (x - i, y))
+getNextPos (E, (x, y)) (R i) = (S, (x, y - i))
+getNextPos (E, (x, y)) (L i) = (N, (x, y + i))
+getNextPos (S, (x, y)) (R i) = (W, (x - i, y))
+getNextPos (S, (x, y)) (L i) = (E, (x + i, y))
+getNextPos (W, (x, y)) (R i) = (N, (x, y + i))
+getNextPos (W, (x, y)) (L i) = (S, (x, y - i))
+
+getHqDist :: String -> Int
+getHqDist [] = 0
+getHqDist ss = let (d, (fx, fy)) = List.foldl getNextPos (N, (0,0)) $ parseInput ss
+                in (abs fx) + (abs fy)
+
--- test/Day1Spec.hs
+++ test/Day1Spec.hs
@@ -6,10 +6,31 @@ import Day1 as Day1
 spec :: Spec
 spec = do
   describe "Day1" $ do
-    context "t1" $ do
-      it "t1 should return 1" $ do
-        Day1.t1 1 `shouldBe` 1
+    context "parseStep" $ do
+      it "should parse a left step" $ do
+        Day1.parseStep "L1" `shouldBe` Day1.L 1
+
+      it "should parse a right step" $ do
+        Day1.parseStep "R100" `shouldBe` Day1.R 100
+
+    context "parseInput" $ do
+      it "should parse a list of steps" $ do
+        Day1.parseInput "R1, L1, R2, R3, L2" `shouldBe` [Day1.R 1, Day1.L 1, Day1.R 2, Day1.R 3, Day1.L 2]
+
+    context "getHqDist" $ do
+      it "should return 5 for R2, L3" $ do
+        Day1.getHqDist "R2, L3" `shouldBe` 5
+
+      it "should return 2 for R2, R2, R2" $ do
+        Day1.getHqDist "R2, R2, R2" `shouldBe` 2
+
+      it "should return 12 for R5, L5, R5, R3" $ do
+        Day1.getHqDist "R5, L5, R5, R3" `shouldBe` 12
+
+      it "should return 239 for day1Input" $ do
+        Day1.getHqDist day1Input `shouldBe` 239
 
 main :: IO ()
 main = hspec spec
 
+day1Input = "R2, L5, L4, L5, R4, R1, L4, R5, R3, R1, L1, L1, R4, L4, L1, R4, L4, R4, L3, R5, R4, R1, R3, L1, L1, R1, L2, R5, L4, L3, R1, L2, L2, R192, L3, R5, R48, R5, L2, R76, R4, R2, R1, L1, L5, L1, R185, L5, L1, R5, L4, R1, R3, L4, L3, R1, L5, R4, L4, R4, R5, L3, L1, L2, L4, L3, L4, R2, R2, L3, L5, R2, R5, L1, R1, L3, L5, L3, R4, L4, R3, L1, R5, L3, R2, R4, R2, L1, R3, L1, L3, L5, R4, R5, R2, R2, L5, L3, L1, L1, L5, L2, L3, R3, R3, L3, L4, L5, R2, L1, R1, R3, R4, L2, R1, L1, R3, R3, L4, L2, R5, R5, L1, R4, L5, L5, R1, L5, R4, R2, L1, L4, R1, L1, L1, L5, R3, R4, L2, R1, R2, R1, R1, R3, L5, R1, R4"
