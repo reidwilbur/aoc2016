a1dd85bb495fafeff9dc2834f8dcada1ccfd18ae
Reid Wilbur <reid.wilbur@gmail.com>
2016-12-03 23:37:26 -0500

    Day2 part 1 sln

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,9 +1,21 @@
 module Day2 (
   parseInstrs,
+  getCode,
+  getFinalPosition,
   Instr(U,L,D,R)
   ) where
 
 data Instr = U | L | D | R deriving(Eq, Show)
+
+type Position = (Int, Int)
+
+nextPos :: Instr -> Position -> Position
+nextPos instr pos@(r, c) =
+  case (instr) of
+    U -> if (r > 0) then (r - 1, c) else pos
+    D -> if (r < 2) then (r + 1, c) else pos
+    L -> if (c > 0) then (r,     c - 1) else pos
+    R -> if (c < 2) then (r,     c + 1) else pos
 
 parseChar :: Char -> Instr
 parseChar c =
@@ -15,8 +27,16 @@ parseChar c =
     _ -> error ("Can't parse char " ++ show c)
 
 parseLine :: String -> [Instr]
-parseLine s = foldr (\c il -> (parseChar c):il) [] s
+parseLine = foldr (\c il -> (parseChar c):il) []
 
 parseInstrs :: [String] -> [[Instr]]
-parseInstrs ss = foldr (\s ils -> (parseLine s):ils) [] ss
+parseInstrs = foldr (\s ils -> (parseLine s):ils) []
 
+getFinalPosition :: Position -> [Instr] -> Position
+getFinalPosition p = head . foldl (\poss instr -> (nextPos instr (head poss)):poss) [p]
+
+getPositions :: [[Instr]] -> [Position]
+getPositions = reverse . init . foldl (\poss instrs -> (getFinalPosition (head poss) instrs):poss) [(1,1)]
+
+getCode :: [[Instr]] -> [Int]
+getCode = map (\(r, c) -> (r * 3) + c + 1) . getPositions
--- test/Day2Spec.hs
+++ test/Day2Spec.hs
@@ -10,6 +10,26 @@ spec = do
       it "should return correct result" $ do
         parseInstrs ["UDRL", "DDUULLRR"] `shouldBe` [[U,D,R,L],[D,D,U,U,L,L,R,R]]
 
+    context "getFinalPosition" $ do
+      it "should return (0,0) for ULL starting at (1,1)" $ do
+        getFinalPosition (1,1) [U,L,L] `shouldBe` (0,0)
+
+      it "should return (2,2) for RRDD starting at (0,0)" $ do
+        getFinalPosition (0,0) [R,R,D,D] `shouldBe` (2,2)
+
+      it "should return (2,1) for LURDL starting at (2,2)" $ do
+        getFinalPosition (2,2) [L,U,R,D,L] `shouldBe` (2,1)
+
+      it "should return (1,1) for UUUUD starting at (2,1)" $ do
+        getFinalPosition (2,1) [U,U,U,U,D] `shouldBe` (1,1)
+
+    context "getCode" $ do
+      it "should return correct result" $ do
+        getCode (parseInstrs ["ULL","RRDD","LURDL","UUUUD"]) `shouldBe` [1,9,8,5]
+
+      it "should return correct result for day2Input" $ do
+        getCode (parseInstrs day2Input) `shouldBe` [9,5,5,4,9]
+
 main :: IO ()
 main = hspec spec
 
